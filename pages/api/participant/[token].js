57d5ecce7976fc4557132aa937c902cd596c9884
vg <madhuranand@live.in>
2026-05-10 09:41:25 +0530

    Update [token].js

--- pages/api/participant/[token].js
+++ pages/api/participant/[token].js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     const accessToken = await getAccessToken()
 
     const zohoResponse = await axios.get(
-      "https://www.zohoapis.in/creator/v2.1/data/madhur_moodforest755/moodforest-app/report/API_Test/all",
+      "https://www.zohoapis.in/creator/v2.1/data/madhur_moodforest755/moodforest-app/report/API_Test/records",
       {
         headers: {
           Authorization: `Zoho-oauthtoken ${accessToken}`
