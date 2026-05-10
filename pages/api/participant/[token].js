bbf6192e49fa53092ce8a428ed412e47b2428f45
vg <madhuranand@live.in>
2026-05-10 09:10:21 +0530

    Update [token].js

--- pages/api/participant/[token].js
+++ pages/api/participant/[token].js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
 
     console.log(process.env.ZOHO_API_DOMAIN) 
     const zohoResponse = await axios.get(
-      `${process.env.ZOHO_API_DOMAIN}/creator/v2/data/${process.env.ZOHO_OWNER_NAME}/${process.env.ZOHO_APP_NAME}/report/${process.env.ZOHO_REPORT_NAME}`,
+      `${process.env.ZOHO_API_DOMAIN}/creator/v2/data/${process.env.ZOHO_OWNER_NAME}/${process.env.ZOHO_APP_NAME}/report/${process.env.ZOHO_REPORT_NAME}/all`,
       {
         headers: {
           Authorization: `Zoho-oauthtoken ${accessToken}`
