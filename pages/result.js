eafd44cb5160a206183d2ea80515947ae5ca27c9
vg <madhuranand@live.in>
2026-05-21 11:44:29 +0530

    Fixed dossier route

--- pages/result.js
+++ pages/result.js
@@ -113,9 +113,9 @@ export default function ResultPage() {
       Integrated Interpretation
     </div>
 
-    <div className="text-[14px] leading-[1.95] text-[#64776d] whitespace-pre-line">
-      {reflection}
-    </div>
+    <div className="text-[14px] leading-[1.95] text-[#64776d]">
+  Test reflection
+</div>
 
   </div>
 
